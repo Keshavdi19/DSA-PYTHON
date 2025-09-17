849e3446564b79dc05b5f31d14203c883493da38
Keshavdi19 <165756172+Keshavdi19@users.noreply.github.com>
2025-09-17 14:45:32 +0530

    first commit

--- day4recursion/leetcodeques/count.py
+++ day4recursion/leetcodeques/count.py
@@ -5,3 +5,8 @@ while num > 0:
     count += 1
     num = num // 10
 print("The number of digits in", n, "is", count)
+# one more way to solve this problem
+from math import *
+def count_digits(num):
+    return int(log10(num)) + 1
+print("The number of digits in", n, "is", count_digits(n))
